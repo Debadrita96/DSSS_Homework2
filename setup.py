44e44807915ff188c982bf6a7ae2fc23c27c98c6
Debadrita <debadrita.mukherjee@fau.de>
2023-11-13 19:39:06 +0100

    setup for file

--- setup.py
+++ setup.py
@@ -2,13 +2,23 @@ from setuptools import setup, find_packages
 
 setup(
     name='math_quiz',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
+        # List your dependencies here
+        # For example, if you use random, it's already included in Python
     ],
     entry_points={
         'console_scripts': [
-            'math_quiz=math_quiz.math_quiz:main',
+            'math_quiz = math_quiz.math_quiz:main',
         ],
     },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
+
